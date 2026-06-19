88b1986d3f03b1e4c469557297e1a5bc40d4e49f
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2026-06-19 08:53:16 +0600

    feat(task) : implement add/edit functionality handlers

---
+++ js/components/taskModal.js
@@ -0,0 +1,70 @@
+import { addTask, editTask } from "../modules/tasks.js";
+import { renderKanban } from "../pages/tasksPage.js";
+
+const modal = document.getElementById("taskModal");
+const form = document.getElementById("taskForm");
+const closeBtn = document.getElementById("closeTaskModal");
+const cancelBtn = document.getElementById("cancelTaskModal");
+
+export function openTaskModal(task = null) {
+  modal.classList.remove("hidden");
+
+  const title = document.getElementById("taskModalTitle");
+  const SaveBtn = document.getElementById("saveTaskModal");
+
+  if (task) {
+    title.textContent = "Edit Task";
+    SaveBtn.textContent = "update Task";
+    form.dataset.editId = task.id;
+
+    form.taskTitle.value = task.title || "";
+    form.taskDescription.value = task.description || "";
+    form.taskStatus.value = task.status || "todo";
+    form.taskPriority.value = task.priority || "medium";
+    form.taskDueDate.value = task.dueDate || "";
+  } else {
+    title.textContent = "Add Task";
+    SaveBtn.textContent = "Save Task";
+    delete form.dataset.editId;
+    form.reset();
+  }
+}
+
+export function closeTaskModal() {
+  modal.classList.add("hidden");
+}
+
+// Handle submit
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const taskData = {
+      id: form.dataset.editId ? Number(form.dataset.editId) : Date.now(),
+      title: form.taskTitle.value.trim(),
+      description: form.taskDescription.value.trim(),
+      status: form.taskStatus.value,
+      priority: form.taskPriority.value,
+      dueDate: form.taskDueDate.value,
+      createdAt: new Date().toISOString(),
+    };
+
+    if (!taskData.title) {
+      alert("Task must have a title");
+      return;
+    }
+
+    if (form.dataset.editId) {
+      editTask(taskData.id, taskData);
+    } else {
+      addTask(taskData);
+    }
+
+    closeTaskModal();
+    form.reset();
+    renderKanban();
+  });
+}
+
+if (closeBtn) closeBtn.addEventListener("click", closeTaskModal);
+if (cancelBtn) cancelBtn.addEventListener("click", closeTaskModal);
