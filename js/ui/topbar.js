02af0bc6273bd9bd5d5d09fd8a699365ed9c873b
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2026-06-20 11:22:55 +0600

    fix: solve icons swap

--- js/ui/topbar.js
+++ js/ui/topbar.js
@@ -1,6 +1,6 @@
 export function initTopbar() {
   const toggleBtn = document.getElementById("themeToggle");
-  const icon = document.getElementById("themIcon");
+  const icon = document.getElementById("themeIcon");
   const root = document.documentElement;
 
   if (!toggleBtn || !icon) return;
@@ -9,16 +9,21 @@ export function initTopbar() {
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "dark") {
     root.classList.add("dark");
-    icon.classList.replace("bx-sun", "bx-moon");
+    icon.classList.remove("bx-sun");
+    icon.classList.add("bx-moon");
   }
 
   // Toggle event
   toggleBtn.addEventListener("click", () => {
     const isDark = root.classList.toggle("dark");
     localStorage.setItem("theme", isDark ? "dark" : "light");
-    icon.classList.replace(
-      isDark ? "bx-sun" : "bx-moon",
-      isDark ? "bx-moon" : "bx-sun",
-    );
+
+    if (isDark) {
+      icon.classList.remove("bx-sun");
+      icon.classList.add("bx-moon");
+    } else {
+      icon.classList.remove("bx-moon");
+      icon.classList.add("bx-sun");
+    }
   });
 }
